7cc814d8d5fcaa76d25152b9ef9aab173751d57c
Kristina <KristinaDarawshe@users.noreply.github.com>
2017-06-18 13:09:49 +0300

    n

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InGameMenu : MonoBehaviour {
-	public GameObject panel1;
 
     public void PauseGame()
     {
+
         Time.timeScale = 0;
     }
 
     public void UnpauseGame()
     {
         Time.timeScale = 1;
-		panel1.SetActive (false);
     }
 
 	public void ExitGame()
 	{
+
 		Debug.Log ("quit");
 		Application.Quit();
 	}
